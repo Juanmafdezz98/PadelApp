32d6031649a4b00fdc202dd79c94f3e44317f81f
Juanmafdezz98 <juanmanuelfdezz@gmail.com>
2024-03-14 01:41:30 +0100

    wallpaper

--- app/src/main/java/com/example/padelapp/LoginActivity.kt
+++ app/src/main/java/com/example/padelapp/LoginActivity.kt
@@ -1,44 +1,20 @@
 package com.example.padelapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btStart: Button
-
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        //Init all components
-        initComponents()
-
-        //Init all listener for objects
-        initListener()
-    }
-
-    private fun initListener() {
-        btStart.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun initComponents() {
-        btStart = findViewById(R.id.btStart)
     }
 }
