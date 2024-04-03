31533e357fce34d134e15e4daf8236b1387218ac
Juanmafdezz98 <juanmanuelfdezz@gmail.com>
2024-04-04 00:06:51 +0200

    Pantalla de Connect - xml

--- app/src/main/java/com/example/padelapp/MainActivity.kt
+++ app/src/main/java/com/example/padelapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.padelapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
