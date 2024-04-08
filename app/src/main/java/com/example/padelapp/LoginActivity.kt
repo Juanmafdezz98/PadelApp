c173256ef1387859dfa05d1049a06d46af59e112
Juanmafdezz98 <juanmanuelfdezz@gmail.com>
2024-04-09 01:01:46 +0200

    Next button + Toasts

--- app/src/main/java/com/example/padelapp/LoginActivity.kt
+++ app/src/main/java/com/example/padelapp/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.example.padelapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
@@ -26,8 +29,10 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var etIP: EditText
     private lateinit var btConnect: Button
     private lateinit var tvConnect: TextView
-    private lateinit var btSend : Button
-    private lateinit var socket : Socket
+    private lateinit var fabNext: FloatingActionButton
+    private lateinit var btSend: Button
+    private lateinit var socket: Socket
+    private val context = this@LoginActivity
     private val port: Int = 8080
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,11 +51,25 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private suspend fun connect(address: InetAddress) {
-        withContext(Dispatchers.IO) {
+        withContext(IO) {
             try {
                 socket = Socket(address, port)
             } catch (e: Exception) {
                 Log.i("Socket", "Exception")
+                /*withContext(Dispatchers.Main) {
+                    Toast.makeText(
+                        context,
+                        "Unable to establish connection with the provided IP address. Please try again.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }*/
+                context.runOnUiThread {
+                    Toast.makeText(
+                        context,
+                        "Unable to establish connection with the provided IP address. Please try again.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
                 e.printStackTrace()
             }
         }
@@ -64,22 +83,30 @@ class LoginActivity : AppCompatActivity() {
             CoroutineScope(IO).launch {
                 connect(ip)
             }
+
         }
 
+
         btSend.setOnClickListener {
-            if(::socket.isInitialized) {
-                GlobalScope.launch(Dispatchers.IO) {
+            if (::socket.isInitialized) {
+                GlobalScope.launch(IO) {
                     val outPutStream = OutputStreamWriter(socket.getOutputStream())
                     outPutStream.write("HELLO SERVER")
                     outPutStream.flush()
 
-                    launch(Dispatchers.Main){
+                    launch(Dispatchers.Main) {
                         tvConnect.setText(R.string.received)
                     }
                 }
             }
+            else noConnected()
         }
 
+        fabNext.setOnClickListener {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            if (::socket.isInitialized) startActivity(intent)
+            noConnected()
+        }
     }
 
     private fun initComponents() {
@@ -87,5 +114,15 @@ class LoginActivity : AppCompatActivity() {
         etIP = findViewById(R.id.ip_add)
         tvConnect = findViewById(R.id.tv_connect)
         btSend = findViewById(R.id.btSend)
+        fabNext = findViewById(R.id.btnForward)
     }
+
+    private fun noConnected() {
+        Toast.makeText(
+            this,
+            "Connection to the server has not been established. Please connect first",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
 }
